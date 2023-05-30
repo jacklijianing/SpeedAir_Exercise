e5d78f543447ea75491c177fde22f6cb5f71c353
Jianing <jacklijianing@gmail.com>
2023-05-30 13:31:20 -0400

    First Commit

---
+++ SpeedAir_Exercise/SpeedAir_Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeedAir_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
